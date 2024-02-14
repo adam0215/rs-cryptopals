0c36408536b96956d838f901e38a300e4916a5e3
Adam Gustafsson <adam.gust@hotmail.se>
2024-02-14 19:51:18 +0100

    Complete challenge 3

---
+++ src/bin/ch3.rs
@@ -0,0 +1,35 @@
+use rs_cryptopals::hex::hex_decode;
+use std::collections::HashMap;
+
+const INPUT: &[u8] = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+fn main() {
+    let decoded_hex = hex_decode(INPUT).unwrap();
+
+    let (key, _) = (0..=255)
+        .map(|i| {
+            let xored: Vec<_> = decoded_hex.iter().map(|ch| ch ^ i).collect();
+            let scored = score_text(xored.as_slice());
+
+            (i, scored)
+        })
+        .collect::<HashMap<u8, usize>>()
+        .into_iter()
+        .max_by(|a, b| a.1.cmp(&b.1))
+        .unwrap();
+
+    let deciphered_str: String = decoded_hex.iter().map(|ch| (*ch ^ key) as char).collect();
+
+    println!("Cipher key: {}", key as char);
+    println!("{}", deciphered_str);
+}
+
+fn score_text(text: &[u8]) -> usize {
+    // Scoring table based on hint in challenge
+    let scoring_ref = [
+        b'e', b't', b'a', b'o', b'i', b'n', b' ', b's', b'h', b'r', b'd', b'l', b'u',
+    ];
+
+    text.iter()
+        .fold(0, |acc, ch| acc + (scoring_ref.contains(ch) as usize))
+}
